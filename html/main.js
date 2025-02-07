14d16b859f9d0099b3faa42556b007b695d3b6b6
Kamzie1 <kamilzie07@gmail.com>
2025-02-07 19:47:37 +0100

    ulepszona wersja bez symulacji, dodano usuwanie i uporządkowano kod

--- html/main.js
+++ html/main.js
@@ -1,3 +1,5 @@
+/// Canvas
+
 let canvas = document.getElementById('canvas');
 canvas.style.display='block';
 canvas.style.width='100%';
@@ -10,26 +12,16 @@ let WYS_j = WYSOKOSC/100;
 let WYS_J = WYSOKOSC/10; 
 const ctx = canvas.getContext("2d");
 
-///
+/// zmienne globalne
 
 const soczewkaSkupiajaca = {nazwa: "SoczS", typ: "SoczS", wspx: SZEROKOSC/2, h: WYSOKOSC/4, F: 1, id: 0};
-const promienSwietlny = {nazwa: "PromS", typ: "PromS", wspx: SZEROKOSC/4, wspy: WYSOKOSC/8, alfa: 0, id: 0};
+const promienSwietlny = {nazwa: "PromS", typ: "PromS", wspx: SZEROKOSC/4, wspy: WYSOKOSC/3, alfa: 0, id: 0};
+
 let os_Optyczna;
+let wstazka;
+let id_Obiektu=-1;
 
-function zaladujOs(){
-    if(localStorage.getItem('os_Optyczna'))
-        {
-            os_Optyczna = JSON.parse(localStorage.getItem('os_Optyczna'));
-        }
-        else
-        {
-            os_Optyczna =[];
-            localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
-        }
-}
-
-
-///
+const kontener = document.getElementById("trescWstazki");
 
 let Tworzenie = `<div class='soczewki'>
                     <button class='skupiajaca' id='skupiajaca'>Skupiająca</button>
@@ -59,10 +51,69 @@ let Symulacja = `<div class="sterowanie" id="sterowanie">
 
                 </div>
                 `;
-let wstazka;
-let id_Obiektu=-1;
-const kontener = document.getElementById("trescWstazki");
 
+/// Główna pętla
+
+rysuj();
+
+function rysuj(){
+    usunLocalStorage();
+    ctx.clearRect(0, 0, SZEROKOSC, WYSOKOSC);
+    rysuj_os();
+    rysuj_obiekty();
+}
+
+/// Funkcje odświeżania
+
+window.onload = function(){
+    usunLocalStorage();
+    zaladujWstazke();
+    zaladujOs();
+    dodawanieEventListener();
+    wyswietlWstazke(wstazka);
+}
+
+function usunLocalStorage(){
+    if (!sessionStorage.getItem("sessionVisit")) {
+        localStorage.removeItem('wstazka');
+        localStorage.removeItem('os_Optyczna');
+        localStorage.removeItem('id_Obiektu');
+        sessionStorage.setItem("sessionVisit", "true");
+        location.reload();
+    } 
+}
+
+function odswiez_os_optyczna(){
+    zaladujOs();
+    for(let i=0;i<os_Optyczna.length;i++)
+    {
+        os_Optyczna[i].id = i;
+    }
+    localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+}
+
+function zaladujAktualneId(){
+    if(localStorage.getItem('id_Obiektu'))
+        {
+            id_Obiektu = localStorage.getItem('id_Obiektu');
+        }
+    else
+        {
+            id_Obiektu=-1;
+        }
+}
+
+function zaladujOs(){
+    if(localStorage.getItem('os_Optyczna'))
+        {
+            os_Optyczna = JSON.parse(localStorage.getItem('os_Optyczna'));
+        }
+        else
+        {
+            os_Optyczna =[];
+            localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        }
+}
 
 function zaladujWstazke()
 {
@@ -77,49 +128,55 @@ function zaladujWstazke()
     }
 }
 
-function wyczysc(){
-    os_Optyczna=[];
-    localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
-    localStorage.setItem('id_Obiektu', -1);
-    location.reload();
-    rysuj();
+/// Funckje rysowania
+
+function rysuj_soczS(id){
+    if (!os_Optyczna[id]) return;
+    ctx.beginPath();
+    ctx.lineWidth=2;
+    ctx.moveTo(os_Optyczna[id].wspx,WYSOKOSC/2-os_Optyczna[id].h);
+    ctx.lineTo(os_Optyczna[id].wspx, WYSOKOSC/2+ os_Optyczna[id].h);
+    ctx.stroke();
 }
 
-function dodajPrzycisk(id){
-    let przycisk = document.createElement("button");
-    przycisk.innerText = `${id} : ${os_Optyczna[id].nazwa}`;
-    przycisk.classList.add("przyciskObiektu");
+function rysuj_promS(id){
+    if (!os_Optyczna[id]) return;
+    ctx.beginPath();
+    ctx.lineWidth=2;
+    let dl = 30;
+    ctx.moveTo(os_Optyczna[id].wspx, os_Optyczna[id].wspy);
+    ctx.arc(os_Optyczna[id].wspx, os_Optyczna[id].wspy, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.moveTo(os_Optyczna[id].wspx, os_Optyczna[id].wspy);
 
-    przycisk.onclick = function(){
-        wyswietlWlasciwosci(id);
-    }
-    document.getElementById('lista-obiektow').appendChild(przycisk);
+    ctx.lineTo(os_Optyczna[id].wspx +dl*Math.cos(os_Optyczna[id].alfa*Math.PI/180), os_Optyczna[id].wspy - dl*Math.sin(os_Optyczna[id].alfa*Math.PI/180));
+    ctx.stroke();
 }
 
-function zaladujSymulacje(){
-    kontener.innerHTML=Symulacja;
+function rysuj_os(){
+    ctx.beginPath();
+    ctx.lineWidth=3;
+    ctx.moveTo(0,WYSOKOSC/2);
+    ctx.lineTo(SZEROKOSC, WYSOKOSC/2);
+    ctx.stroke();
+}
+
+function rysuj_obiekty(){
     zaladujOs();
-    for(let i=0;i<os_Optyczna.length;i++)
-    {
-        dodajPrzycisk(i);
-    }
-    document.getElementById('wyczysc').addEventListener('click', function(){
-        wyczysc();
-    });
+    for(let i=0;i<os_Optyczna.length; i++)
+        {
+            if(os_Optyczna[i].typ=='SoczS')
+            {
+                rysuj_soczS(i);
+            }
+            else if(os_Optyczna[i].typ=='PromS')
+            {
+                rysuj_promS(i);
+            }
+        }
 }
 
-function zaladujTworzenie(){
-    kontener.innerHTML= Tworzenie;
-    document.getElementById('skupiajaca').addEventListener('click', function(){
-        zaktualizujOs(soczewkaSkupiajaca);
-        console.log(os_Optyczna);
-    });
-    
-    document.getElementById('promien-swietlny').addEventListener('click', function(){
-        zaktualizujOs(promienSwietlny);
-        console.log(os_Optyczna);
-    });
-}
+/// Funkcje obsługi wstążek
 
 function wyswietlWstazke(wstazka){
     zaladujAktualneId();
@@ -143,24 +200,6 @@ function wyswietlWstazke(wstazka){
     }
 }
 
-function usunLocalStorage(){
-    if (!sessionStorage.getItem("sessionVisit")) {
-        localStorage.removeItem('wstazka');
-        localStorage.removeItem('os_Optyczna');
-        localStorage.removeItem('id_Obiektu');
-        sessionStorage.setItem("sessionVisit", "true");
-        location.reload();
-    } 
-}
-
-window.onload = function(){
-    usunLocalStorage();
-    zaladujWstazke();
-    zaladujOs();
-    dodawanieEventListener();
-    wyswietlWstazke(wstazka);
-}
-
 function dodawanieEventListener(){
     document.getElementById('Opcja-symulacji').addEventListener('click', function() {
         wstazka = "SYMULACJA";
@@ -175,16 +214,64 @@ function dodawanieEventListener(){
     });
 }
 
-function zaladujAktualneId(){
-    if(localStorage.getItem('id_Obiektu'))
-        {
-            id_Obiektu = localStorage.getItem('id_Obiektu');
-        }
-    else
-        {
-            id_Obiektu=-1;
-        }
+/// Funckje obsługi Symulacji
+
+function zaladujSymulacje(){
+    kontener.innerHTML=Symulacja;
+    document.getElementById('lista-obiektow').innerHTML='';
+    zaladujOs();
+    for(let i=0;i<os_Optyczna.length;i++)
+    {
+        dodajPrzycisk(i);
+    }
+    document.getElementById('wyczysc').addEventListener('click', function(){
+        wyczysc();
+    });
 }
+
+function wyczysc(){
+    os_Optyczna=[];
+    localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+    localStorage.setItem('id_Obiektu', -1);
+    location.reload();
+    rysuj();
+}
+
+function dodajPrzycisk(id){
+    let przycisk = document.createElement("button");
+    przycisk.innerText = `${id} : ${os_Optyczna[id].nazwa}`;
+    przycisk.classList.add("przyciskObiektu");
+
+    przycisk.onclick = function(){
+        wyswietlWlasciwosci(id);
+    }
+    document.getElementById('lista-obiektow').appendChild(przycisk);
+}
+
+/// Funkcje obsługi Tworzenia
+
+function zaladujTworzenie(){
+    kontener.innerHTML= Tworzenie;
+    document.getElementById('skupiajaca').addEventListener('click', function(){
+        zaktualizujOs(soczewkaSkupiajaca);
+    });
+    
+    document.getElementById('promien-swietlny').addEventListener('click', function(){
+        zaktualizujOs(promienSwietlny);
+    });
+}
+
+function zaktualizujOs(obiekt){
+    obiekt.id = os_Optyczna.length;
+    localStorage.setItem('id_Obiektu', obiekt.id)
+    os_Optyczna.push(obiekt);
+
+    localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+    wyswietlWlasciwosci(os_Optyczna.length-1);
+    rysuj();
+}
+
+/// Funkcje obsługi Właściwości
 
 function zaladujWstazkeWlasciwosci(){
     if(!document.getElementById('Opcja-wlasciwosci'))
@@ -250,6 +337,9 @@ function zaladujWlasciwosciSoczewki(id){
                                 <label for='F'>F: </label>
                                 <input type='text'' id='F' placeholder='podaj F:' value=${os_Optyczna[id].F}>
                             </form>
+                        </div>
+                        <div class='dod_przyciski' id='dod_przyciski'>
+                            <button class="usun" id="usun">Usuń</button>
                         </div>`;
     dodawanieEventListener();
     EventSoczS(id);
@@ -267,8 +357,12 @@ function zaladujWlasciwosciPromienia(id){
                                 <label for='alfa'>alfa: </label>
                                 <input type='text'' id='alfa' placeholder='podaj alfa:' value=${os_Optyczna[id].alfa}>
                             </form>
+                        </div>
+                        <div class='dod_przyciski' id='dod_przyciski'>
+                            <button class="usun" id="usun">Usuń</button>
                         </div>`;
     dodawanieEventListener();
+    EventPromS(id);
 }
 
 function EventSoczS(id){
@@ -280,84 +374,76 @@ function EventSoczS(id){
     });
 
     document.getElementById('wspx').addEventListener("input", function(){
-        os_Optyczna[id].wspx = this.value;
+        os_Optyczna[id].wspx = parseFloat(this.value) || 0;
         localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
         rysuj();
     });
 
     document.getElementById('h').addEventListener("input", function(){
-        os_Optyczna[id].h = this.value;
+        os_Optyczna[id].h = parseFloat(this.value) || 0;
         localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
         rysuj();
     });
 
     document.getElementById('F').addEventListener("input", function(){
-        os_Optyczna[id].F = this.value;
+        os_Optyczna[id].F = parseFloat(this.value) || 0;
         localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
         rysuj();
     });
 
+    document.getElementById('usun').addEventListener("click", function(){
+        wstazka = "SYMULACJA";
+        localStorage.setItem('wstazka', wstazka);
+        wyswietlWstazke(wstazka);
+        os_Optyczna.splice(id, 1);
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        odswiez_os_optyczna();
+        localStorage.setItem('id_Obiektu', -1);
+        location.reload();
+        rysuj();
+    });
+
 }
+
+function EventPromS(id){
+
+    document.getElementById('nazwa').addEventListener("input", function(){
+        os_Optyczna[id].nazwa = this.value;
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        rysuj();
+    });
+
+    document.getElementById('wspx').addEventListener("input", function(){
+        os_Optyczna[id].wspx = parseFloat(this.value) || 0;
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        rysuj();
+    });
+
+    document.getElementById('wspy').addEventListener("input", function(){
+        os_Optyczna[id].wspy = parseFloat(this.value) || 0;
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        rysuj();
+    });
+
+    document.getElementById('alfa').addEventListener("input", function(){
+        os_Optyczna[id].alfa = parseFloat(this.value) || 0;
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        rysuj();
+    });
+
+    document.getElementById('usun').addEventListener("click", function(){
+        wstazka = "SYMULACJA";
+        localStorage.setItem('wstazka', wstazka);
+        wyswietlWstazke(wstazka);
+        os_Optyczna.splice(id, 1);
+        localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
+        odswiez_os_optyczna();
+        localStorage.setItem('id_Obiektu', -1);
+        location.reload();
+        rysuj();
+    });
+}
+
 ///
 
-function zaktualizujOs(obiekt){
-    obiekt.id = os_Optyczna.length;
-    localStorage.setItem('id_Obiektu', obiekt.id)
-    os_Optyczna.push(obiekt);
 
-    localStorage.setItem('os_Optyczna', JSON.stringify(os_Optyczna));
-    wyswietlWlasciwosci(os_Optyczna.length-1);
-    rysuj();
-}
-
-function rysuj_soczS(id){
-    if (!os_Optyczna[id]) return;
-    ctx.beginPath();
-    ctx.lineWidth=2;
-    ctx.moveTo(os_Optyczna[id].wspx,WYSOKOSC/2);
-    ctx.lineTo(os_Optyczna[id].wspx, WYSOKOSC/2-os_Optyczna[id].h);
-    ctx.moveTo(os_Optyczna[id].wspx,WYSOKOSC/2);
-    ctx.lineTo(os_Optyczna[id].wspx, WYSOKOSC/2+os_Optyczna[id].h);
-    ctx.stroke();
-}
-
-function rysuj_promS(id){
-    if (!os_Optyczna[id]) return;
-    ctx.beginPath();
-    ctx.lineWidth=2;
-    ctx.arc(os_Optyczna[id].wspx, os_Optyczna[id].wspy, 2, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.lineTo(os_Optyczna[id].wspx +30, os_Optyczna[id].wspy);
-    ctx.stroke();
-}
-
-function rysuj_os(){
-    ctx.beginPath();
-    ctx.lineWidth=3;
-    ctx.moveTo(0,WYSOKOSC/2);
-    ctx.lineTo(SZEROKOSC, WYSOKOSC/2);
-    ctx.stroke();
-}
-
-function rysuj_obiekty(){
-    zaladujOs();
-    for(let i=0;i<os_Optyczna.length; i++)
-        {
-            if(os_Optyczna[i].typ=='SoczS')
-            {
-                rysuj_soczS(i);
-            }
-            else if(os_Optyczna[i].typ=='PromS')
-            {
-                rysuj_promS(i);
-            }
-        }
-}
-
-function rysuj(){
-    ctx.clearRect(0, 0, SZEROKOSC, WYSOKOSC);
-    rysuj_os();
-    rysuj_obiekty();
-}
-
-rysuj();
